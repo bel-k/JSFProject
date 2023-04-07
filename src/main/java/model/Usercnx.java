cf249a66f0586bf1b69f3266f29aa773cdfc1778
KhaoulaBel <95535082+bel-k@users.noreply.github.com>
2023-04-07 09:21:01 +0000

    index

--- src/main/java/model/Usercnx.java
+++ src/main/java/model/Usercnx.java
@@ -159,7 +159,8 @@ public class Usercnx implements Serializable {
                 String userName=getUsernameFromEmail(email);
                 System.out.println(userName);
                 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("username", userName);
-                return "succes.xhtml?faces-redirect=true";
+
+                return "succes.xhtml";
 
             } else {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Invalid login or password"));
